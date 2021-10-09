20a834f97de7d7c87a6292ba160102f7dd1be9d0
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-10-09 13:20:38 -0400

    added comment

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 module.exports = app
-
+//corinne's comment
 // logging middleware
 app.use(morgan('dev'))
 
