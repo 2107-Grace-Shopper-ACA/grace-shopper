0ffa4e1415834fb46a83867e3cc5679ac746ffe5
green47336 <alex.green407@gmail.com>
2021-10-11 12:53:54 -0400

    changed nav bar header, trying to hook up thunks to front end ui

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
+import {createOrderItem} from '../store/orderItems'
 
-const Products = ({ products }) => {
+const Products = ({ products, createOrderItem }) => {
   return (
     <div id="product-gallery">
       {products.map((product) => {
         return (
           <div key={product.id}>
             <img src={product.imageUrl || "https://i.gifer.com/MNu.gif"}></img>
-            <button>Add to Cart</button>
+            <label htmlFor="product-quantity">Quantity:</label>
+            <input type="number" id="product-quantity" min="1" max="9"/>
+            <button type="button" onClick={() => {createOrderItem({id: product.id, quantity: 1})}}>Add to Cart</button>
           </div>
         );
       })}
@@ -16,6 +19,14 @@ const Products = ({ products }) => {
   );
 };
 
-export default connect((state) => state)(Products);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createOrderItem: (product) => {
+      dispatch(createOrderItem(product))
+    }
+  }
+}
+
+export default connect((state) => state, mapDispatchToProps)(Products);
 
 //Emulating how the Home component is setup with the naming convention and exporting instead of the whole _Products and Products dance we're used to. If this messed up we can fall back on that. - Alex
