5e30d8e68a16345380a31e52cdbae0131c38c7d1
green47336 <alex.green407@gmail.com>
2021-10-16 12:17:30 -0400

    cleaned up Product component and added Order model to get / orderItems

--- client/components/Products.js
+++ client/components/Products.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import {createOrder, createOrderItem, editOrderItem} from '../store'
 
-const Products = ({ products, orders, auth, orderItems, createOrder, createOrderItem, editOrderItem}, state) => {
+const Products = ({ products, orders, auth, orderItems, createOrder, createOrderItem, editOrderItem}) => {
   return (
     <div id="product-gallery">
       {products.map((product) => {
@@ -12,26 +12,28 @@ const Products = ({ products, orders, auth, orderItems, createOrder, createOrder
             <img src={product.imageUrl || "https://i.gifer.com/MNu.gif"}></img>
             <label htmlFor="product-quantity">Quantity:</label>
             <input type="number" id={`${product.id}-quantity`} defaultValue="1" min="1" max="9"/>
-            {/* Hardcoding one for now to test */}
             <button type="button" onClick={(ev) => {
               console.log(document.getElementById(`${product.id}-quantity`).value)
               let cartOrder = orders.find(order => (order.userId === auth.id) && order.isCart)
               
+              //If there is an order that is the cart...
               if(cartOrder){
                 const orderItem = orderItems.find(orderItem => (orderItem.productId === product.id && orderItem.orderId === cartOrder.id))
                 
+                //If there is an orderItem in the cart that matches the orderItem we're trying to add...
                 if(orderItem){
-                  //PUT thunk editOrderItem to reflect updated quantity
                   editOrderItem({ id: orderItem.id, quantity: orderItem.quantity + +document.getElementById(`${product.id}-quantity`).value})
+
+                  //If there ISN'T an orderItem in the cart that matches the orderItem we're trying to add...
                 } else {
-                  createOrderItem({ orderId: cartOrder.id, productId: product.id, quantity: document.getElementById(`${product.id}-quantity`).value})
+                  createOrderItem({ orderId: cartOrder.id, productId: product.id, quantity: +document.getElementById(`${product.id}-quantity`).value})
                 }
+
+                //If there ISN'T an order that is the cart...
               } else {
                 cartOrder = createOrder({userId: auth.id})
-                createOrderItem({ orderId: cartOrder.id, productId: product.id, quantity: document.getElementById(`${product.id}-quantity`).value})
-              }
-              //console.log(`our productz: ${JSON.stringify(products)}`);
-              //console.log(`our state: ${JSON.stringify(state)}`); 
+                createOrderItem({ orderId: cartOrder.id, productId: product.id, quantity: +document.getElementById(`${product.id}-quantity`).value})
+              } 
               }}>Add to Cart</button>
           </div>
         );
@@ -55,5 +57,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect((state) => {console.log(state)
-return state}, mapDispatchToProps)(Products);
+export default connect((state) => state, mapDispatchToProps)(Products);
