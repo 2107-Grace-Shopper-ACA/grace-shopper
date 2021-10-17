78dd2dfa423e877473a10ab2dc501187247e7be8
green47336 <alex.green407@gmail.com>
2021-10-17 12:33:24 -0400

    We now only send back orders that belong to the current user. No guest functionality yet. Still have yet to place this restriction on orderItems.

--- client/store/orders.js
+++ client/store/orders.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const TOKEN = 'token' //They have this in auth so I guess we'll use it even though it'd be easy enough to type out - Alex
+
 /**
  * ACTION TYPES
  */
@@ -16,11 +18,15 @@ import axios from 'axios'
   */
  //same as in orderitems, i think we should be fetch all orders and then work from there? so that way if an admin logs in or something they'll have access to all orders without having to create a separate action ...
  //but again i am confused by what the URIs will be for admin/vs not admin... i think we;ll have to use the authorization token or something in the routes in the api directory - C
- //Eventually we'll want to edit this to be user or admin specific - Alex
  export const loadOrders = () => {
-   console.log('loadOrders fired')
+   const token = window.localStorage.getItem(TOKEN)
+
    return async (dispatch) => {
-     const { data: orders } = await axios.get(`/api/orders`);
+     const { data: orders } = await axios.get(`/api/orders`, {
+      headers: {
+        authorization: token
+      }
+    });
      dispatch(_loadOrders(orders));
    };
  };
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,90 +1,99 @@
 const router = require('express').Router()
-const { models: { Order, OrderItem, User, Product } } = require('../db');
+const {
+  models: { Order, OrderItem, User, Product },
+} = require('../db')
 module.exports = router
 
-//this would be an admin route?
 router.get('/', async (req, res, next) => {
-    //TODO Find out why we get bad token error here - Alex
-    //const user = await User.findByToken(req.headers.authorization)
-    //console.log(`user: ${JSON.stringify(user)}`)
-    try {
-        const orders = await Order.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['id', 'username']
-                },
-                {
-                    model: OrderItem,
-                    include: Product
-                }
-            ]
-        });
-        res.json(orders)
-    } catch (err) {
-        next(err)
+  try {
+    const user = await User.findByToken(req.headers.authorization)
+
+    if (user) {
+      const orders = await Order.findAll({
+        where: {
+            userId: user.id
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['id', 'username'],
+          },
+          {
+            model: OrderItem,
+            include: Product,
+          },
+        ],
+      })
+      console.log(JSON.stringify(orders))
+      res.json(orders)
+    } else {
+        //TODO Avoid an error message in our console if we can't find a user.
+        console.log('No current user found via token.')
     }
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-    try {
-        const _order = await Order.create(req.body);
-        const order = await Order.findByPk(_order.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['id', 'username']
-                },
-                {
-                    model: OrderItem,
-                    include: Product
-                }
-            ]
-        });
-        res.json(order)
-    } catch (err) {
-        next(err)
-    }
+  try {
+    const _order = await Order.create(req.body)
+    const order = await Order.findByPk(_order.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'username'],
+        },
+        {
+          model: OrderItem,
+          include: Product,
+        },
+      ],
+    })
+    res.json(order)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //this will show a specific order ? although this should just be an admin thing?
 //i think i'm getting confused by what the URI's will be for someone who is logged in and for someone who isn't -C
 router.get('/:orderId', async (req, res, next) => {
-    try {
-        const orders = await Order.findByPk(req.params.orderId, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['id', 'username']
-                },
-                {
-                    model: OrderItem,
-                    include: Product
-                }
-            ]
-        })
-        res.json(orders)
-    } catch (err) {
-        next(err)
-    }
+  try {
+    const orders = await Order.findByPk(req.params.orderId, {
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'username'],
+        },
+        {
+          model: OrderItem,
+          include: Product,
+        },
+      ],
+    })
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/users/:userId', async (req, res, next) => {
-    try {
-      const user = await User.findByPk(req.params.userId);
-      const orders = await Order.findAll({
-        where: {
-          userId: user.id
+  try {
+    const user = await User.findByPk(req.params.userId)
+    const orders = await Order.findAll({
+      where: {
+        userId: user.id,
+      },
+      include: [
+        {
+          model: OrderItem,
+          include: Product,
         },
-        include: [
-            {
-                model: OrderItem,
-                include: Product
-            }
-        ]
-      })
-      res.json(orders)
-    } catch (err) {
-      next(err)
-    }
-  })
+      ],
+    })
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
