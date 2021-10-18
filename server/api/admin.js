bfdef06dfa6415df2443c13b4d41836612fa4608
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-18 00:45:57 -0400

    separated admin routes to own file

---
+++ server/api/admin.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const { db } = require('../db');
+const { isLoggedIn, isAdmin } = require('../middleware');
+const { pluralize } = require('inflection');
+
+module.exports = router
+
+Object.entries(db.models).forEach( entry => {
+  const _path = pluralize(entry[0]);
+  const model = entry[1];
+  router.get(`/admin/${_path}`, isLoggedIn, isAdmin, async(req, res, next) => {
+    try {
+      res.send(await model.findAll());
+    } catch (ex) {
+      next(ex);
+    }
+  });
+  router.delete(`/admin/${_path}/:id`, isLoggedIn, isAdmin, async(req, res, next) => {
+    try {
+      const item = await model.findByPk(req.params.id);
+      await item.destroy();
+      res.sendStatus(201);
+    } catch (ex) {
+      next(ex);
+    }
+  });
+});
+ 
--- server/api/index.js
+++ server/api/index.js
@@ -1,31 +1,8 @@
 const router = require('express').Router()
-const { db } = require('../db');
-const { isLoggedIn, isAdmin } = require('../middleware');
-const { pluralize } = require('inflection');
 
 module.exports = router
 
-Object.entries(db.models).forEach( entry => {
-  const _path = pluralize(entry[0]);
-  const model = entry[1];
-  router.get(`/admin/${_path}`, isLoggedIn, isAdmin, async(req, res, next) => {
-    try {
-      res.send(await model.findAll());
-    } catch (ex) {
-      next(ex);
-    }
-  });
-  router.delete(`/admin/${_path}/:id`, isLoggedIn, isAdmin, async(req, res, next) => {
-    try {
-      const item = await model.findByPk(req.params.id);
-      await item.destroy();
-      res.sendStatus(201);
-    } catch (ex) {
-      next(ex);
-    }
-  });
-});
-
+router.use('/admin', require('./admin'))
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 router.use('/orderItems', require('./orderItems'))
