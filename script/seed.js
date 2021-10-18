7267dd1ffa15ab417eb4db1d67399a476bc6dea6
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-18 12:45:56 -0400

    seeded two more products

--- script/seed.js
+++ script/seed.js
@@ -30,8 +30,10 @@ async function seed() {
     Product.create({ name: "Mafaldine", inventory: 100, price: 8, imageUrl: 'https://mobkitchen-objects.imgix.net/recipes/9K8A4161-4.jpg?auto=format&crop=focalpoint&domain=mobkitchen-objects.imgix.net&fit=crop&fp-x=0.5&fp-y=0.5&h=1300&ixlib=php-3.3.0&q=82&w=1300&s=f6d1cff31b151eea765c890308f780a8', description: "Mafaldine, also known as reginette (Italian for little queens), or simply mafalda or mafalde, is a type of ribbon-shaped pasta. It is usually served with a more delicate sauce. Mafaldine were named in honor of Princess Mfalda of Savoy (thus the alernative name of 'little queens')." }),
     Product.create({ name: "Spaghetti", inventory: 100, price: 8, imageUrl: 'https://www.zimbokitchen.com/wp-content/uploads/2012/12/spaghetti2.png', description: "Spaghetti is a long, thin, solid, cylindrical pasta. It is a staple food of traditional Italian cuisine." }),
     Product.create({ name: "Garganelli", inventory: 100, price: 8, imageUrl: 'https://www.qbcucina.com/wp-content/uploads/2020/05/Garganelli-1.jpg', description: "Garganelli are a type of egg-based pasta formed by rolling a flat, square noodle into a cylindrical shape. Garganelli resembles ribbed quills with points at both ends." }),
+    Product.create({ name: "Macaroni", inventory: 100, price: 8, imageUrl: 'https://images.eatthismuch.com/site_media/img/4857_laurabedo_da2c9648-14a9-47fd-bff3-3c1d66ad3fa7.png', description: "Macaroni is shaped like narrow tubes. Made with durum wheat, macaroni is commonly cut in short lengths; curved macaroni may be referred to as elbow macaroni."}),
+    Product.create({ name: "Rigatoni", inventory: 100, price: 8, imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Rigatoni.jpg', description: "Rigatoni are a form of tube-shaped pasta of varying lengths and diameters originating in Italy. They are larger than penne and ziti, and sometimes slightly curved, though not as curved as elbow macaroni."})
   ]);
-  const [penne, mafaldine, spaghetti, garganelli] = products.map(product => product);
+  const [penne, mafaldine, spaghetti, garganelli, macaroni] = products.map(product => product);
 
   //Creating orders
   const orders = await Promise.all([
