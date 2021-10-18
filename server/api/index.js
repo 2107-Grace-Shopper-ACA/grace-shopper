e8b02db1ef84c8133c5ca943b3ee341a538fff87
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-18 00:41:23 -0400

    added pluralize and used db.models to create diff paths with forEach

--- server/api/index.js
+++ server/api/index.js
@@ -1,34 +1,30 @@
 const router = require('express').Router()
-const {models: { Order, User }} = require('../db');
+const { db } = require('../db');
+const { isLoggedIn, isAdmin } = require('../middleware');
+const { pluralize } = require('inflection');
+
 module.exports = router
 
-const isLoggedIn = async (req, res, next) => {
-  try {
-    const user = (await User.findByToken(req.headers.authorization));
-    req.user = user;
-    next()
-  } catch (ex) {
-    next(ex);
-  }
-}
-
-const isAdmin = (req, res, next) => {
-  if(req.user.isAdmin){
-    return next();
-  }
-  const error = new Error('user is not admin');
-  error.status = 401;
-  next(error);
-  
-}
-
-router.get('/admin/orders', isLoggedIn, isAdmin, async (req, res, next) => {
-  try {
-    res.send(await Order.findAll());
-  } catch (ex) {
-    next(ex);
-  }
-})
+Object.entries(db.models).forEach( entry => {
+  const _path = pluralize(entry[0]);
+  const model = entry[1];
+  router.get(`/admin/${_path}`, isLoggedIn, isAdmin, async(req, res, next) => {
+    try {
+      res.send(await model.findAll());
+    } catch (ex) {
+      next(ex);
+    }
+  });
+  router.delete(`/admin/${_path}/:id`, isLoggedIn, isAdmin, async(req, res, next) => {
+    try {
+      const item = await model.findByPk(req.params.id);
+      await item.destroy();
+      res.sendStatus(201);
+    } catch (ex) {
+      next(ex);
+    }
+  });
+});
 
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
