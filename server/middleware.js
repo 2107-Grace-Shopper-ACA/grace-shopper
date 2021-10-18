bb14737c1e17201f25736c0f181554e13741cddf
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-18 00:27:02 -0400

    added middleware.js file and added isLoggedIn to auth

---
+++ server/middleware.js
@@ -0,0 +1,25 @@
+const { models: { User }} = require('./db') 
+
+const isLoggedIn = async (req, res, next) => {
+    try {
+      const user = (await User.findByToken(req.headers.authorization));
+      req.user = user;
+      next()
+    } catch (ex) {
+      next(ex);
+    }
+  }
+  
+const isAdmin = (req, res, next) => {
+    if(req.user.isAdmin){
+        return next();
+    }
+    const error = new Error('user is not admin');
+    error.status = 401;
+    next(error);
+}
+
+module.exports = {
+    isLoggedIn,
+    isAdmin
+}
