b7dbe1c1282d9ad1bf61a5f309d4185cf0434080
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-18 21:23:36 -0400

    added material ui table to products and onSale and isActive to Product model

--- client/components/AdminProducts.js
+++ client/components/AdminProducts.js
@@ -1,15 +1,63 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import { Link, useHistory } from 'react-router-dom';
+import Checkbox from '@material-ui/core/Checkbox';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 /**
  * CLASS COMPONENT
  */
 const AdminProducts = ({products}) => {
-    const history = useHistory();
+
     return (
         <div>
-            <div>
+            <TableContainer component={Paper} style={{width: '100%', overflowX: 'auto'}}>
+                <Table border={2} sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                    <TableRow  style={{backgroundColor:'dodgerblue'}}>
+                        <TableCell></TableCell>
+                        <TableCell>Product Name</TableCell>
+                        <TableCell >Inventory</TableCell>
+                        <TableCell >Price</TableCell>
+                        <TableCell >Active</TableCell>
+                        <TableCell >On Sale</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {products.map((product, idx) => (
+                            <TableRow
+                            border={3}
+                            key={product.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 1 } }}
+                            >
+                                    <TableCell component="th" scope="row">
+                                        {idx + 1}
+                                    </TableCell>
+                                    <TableCell >
+                                        <Link to={`/admin/products/${product.id}`}>
+                                        {product.name}
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell >{product.inventory}</TableCell>
+                                    <TableCell >{product.price}</TableCell>
+                                    <TableCell padding='checkbox'>
+                                        <Checkbox disabled checked={product.isActive} /> 
+                                    </TableCell>
+                                    <TableCell padding='checkbox'>
+                                        <Checkbox disabled checked={product.onSale} /> 
+                                    </TableCell>
+                            </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {/* <div>
 //TODO: add Dialog Box to Add New Product
             </div>
             <div id="product-gallery">
@@ -24,7 +72,7 @@ const AdminProducts = ({products}) => {
                     </div>
                     );
                 })}
-            </div>    
+            </div>     */}
         </div>
     )
 }
