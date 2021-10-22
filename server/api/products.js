a73b6bd3541154f73ea39f5349ad05c0758448e7
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-22 07:14:12 -0400

    gahhhhhhh maybe inventory updates

--- server/api/products.js
+++ server/api/products.js
@@ -23,3 +23,17 @@ router.get('/:productId', async (req, res, next) => {
     next(err)
   }
 })
+
+router.put('/:productId', async (req, res, next) => {
+  const {inventory} = req.body;
+  try {
+    const _product = await Product.findByPk(req.params.productId);
+    await _product.update({..._product, inventory});
+    const product = await Product.findByPk(_product.id, {
+      include: Category
+    });
+    res.json(product);
+  } catch (err) {
+    next(err)
+  }
+})
