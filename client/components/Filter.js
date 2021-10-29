49d57691acbef576c0274f16fc4184d5ac1c2413
green47336 <alex.green407@gmail.com>
2021-10-29 15:20:37 -0400

    starting work on filter bar mui

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -1,31 +1,44 @@
 import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default class Filter extends Component {
-    render() {
-        return (
-            <div className='filter'>
-                <div className='filter-result'>
-                    {this.props.count} Products
-                </div>
-                <div className='filter-sort'>
-                    Sort by{' '}
-                    <select value={this.props.sort} onChange={this.props.sortProducts}>
-                        <option>A-Z</option>
-                        <option value='lowest'>Price Low to High</option>
-                        <option value='highest'>Price High to Low</option>
-                    </select>
-                </div>
-                <div className='filter-category'>
-                    Filter by Category{' '}
-                    <select value={this.props.category} onChange={this.props.filterProducts}>
-                        <option value=''>All</option>
-                        <option value='Small Pastas'>Small Pastas</option>
-                        <option value='Ribbon-Cut'>Ribbon-Cut</option>
-                        <option value='Stuffed'>Stuffed</option>
-                        <option value='Tube-Shaped'>Tube-Shaped</option>
-                    </select>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="filter">
+        <Typography
+          className="filter-result"
+          style={{
+            color: 'white',
+            background: 'linear-gradient(45deg, #ff820d, #f21f2a)',
+            borderRadius: 10,
+            boxShadow: '0 0px 3px 3px #c7570c',
+            padding: '.4rem',
+          }}
+        >
+          Displaying {this.props.count} Products
+        </Typography>
+        <div className="filter-sort">
+          Sort by{' '}
+          <select value={this.props.sort} onChange={this.props.sortProducts}>
+            <option>A-Z</option>
+            <option value="lowest">Price Low to High</option>
+            <option value="highest">Price High to Low</option>
+          </select>
+        </div>
+        <div className="filter-category">
+          Filter by Category{' '}
+          <select
+            value={this.props.category}
+            onChange={this.props.filterProducts}
+          >
+            <option value="">All</option>
+            <option value="Small Pastas">Small Pastas</option>
+            <option value="Ribbon-Cut">Ribbon-Cut</option>
+            <option value="Stuffed">Stuffed</option>
+            <option value="Tube-Shaped">Tube-Shaped</option>
+          </select>
+        </div>
+      </div>
+    )
+  }
 }
