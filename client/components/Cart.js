448fda493a9be41862d26603ffbe2e310f17a789
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-10-29 20:28:27 -0400

    edited cart card

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import { deleteOrderItem, editOrderItem } from '../store';
+import CartAddress from './CartAddress';
+
+
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
@@ -9,12 +12,29 @@ import Typography from '@material-ui/core/Typography'
 import CardContent from '@material-ui/core/CardContent'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
-import CartAddress from './CartAddress';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
+import { createTheme, ThemeProvider, shadows } from '@material-ui/core'
 
   const Cart = ({ orders, orderItems, auth, editOrderItem, deleteOrderItem, user }) => {
     const history = useHistory();
-    
+    const useStyles = makeStyles((theme) => ({
+      root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+      },
+      inline: {
+        display: 'inline',
+      },
+    }));
+
     const EmptyCart = () => {
       return (
         <div className="empty-product">
@@ -91,72 +111,189 @@ const handleSubmit = async() => {
     const onChange = async (ev) => {
       await editOrderItem({id: ev.target.id, quantity: +ev.target.value});
     }
-    
+
+    const classes = useStyles();
     return (
       <Box
         display='flex'
       >
       <Grid className="cart" container style={{margin:'2rem'}} display='flex' direction='column' >
-        <header className="container">
-        <Typography variant="h5" >{auth.username || "Guest"}'s Shopping Cart <span className="count">({totalItems} items)</span></Typography>
-        </header>
-        <Typography variant="h6" style={{color: '#8f8a8a', marginLeft: '1.5rem'}}>Order # {cartOrder.id || "Guest"}</Typography>
+        
+        <Typography 
+          variant="h5" 
+          component='div'
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            padding: '1rem',
+            marginBottom: 0
+          }} 
+        >
+          {auth.username || "Guest"}'s Shopping Cart <span className="count">({totalItems} items)</span>
+        </Typography>
+        
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            background: 'black',
+            borderRadius: 10,
+            boxShadow: '0 0px 7px 7px #ffffff',
+          }}
+        >
+          <Typography 
+            variant="h6" 
+            style={{
+              color: 'darkslategrey',
+              background: 'linear-gradient(45deg, #26b7ff, #28fcdd)',
+              borderRadius: 5,
+              textAlign: 'center',
+              marginBottom: '.5rem'
+            }}
+          >
+            Order # {cartOrder.id || "Guest"}
+          </Typography>
         {
           cartItems.map(item => (
-            <Grid item xs={15} style={{margin: '1rem'}}>
-              <Card color="black">
-                <Box display='flex'>
-                  <CardContent>
-                    <CardMedia
-                      component="img"
-                      style={{height: 100, width: 150}}
-                      image={item.product.imageUrl || "https://i.gifer.com/MNu.gif"}
-                      alt="product image"
-                    />
-                    <Typography variant='h6'>
-                    {item.product.name}  ({item.quantity})
-                      
-                    </Typography>
-                  </CardContent>
-                  <CardContent>
-                    <Typography variant='subtitle1' color="textSecondary">
-                    <p>Price:</p>
-                    <br></br>
-                    <p>${item.product.price}</p>
+            <>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                background: 'black',
+                
+              }}
+            >
+              <div
+                style={{
+                  margin: '1rem'
+                }}
+              >
+                <Typography
+                  style={{
+                    color: 'slategrey'
+                  }}
+                >
+                  ID# {item.id}
+                </Typography>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <div>
+                  <img 
+                    src={item.product.imageUrl}
+                    alt='product image'
+                    style={{
+                      maxWidth: '130px',
+                      border: '1px solid white',
+                      borderRadius: 10,
+                      boxShadow: '0 0px 3px 3px #20c9c9',
+                      marginBottom: '.5rem',
+                      marginLeft: '1rem'
+                    }}
+                  />
+                  <br></br>
+                  <Typography
+                    style={{
+                      marginLeft: '1rem'
+                    }}
+                  >
+                    {item.product.name}
+                  </Typography>
+                  <br></br>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between'
+                    }}
+                  >
+                    <Typography>
+                      Price:
                     </Typography>
                     <Typography>
-                    <br></br>
-                    <br></br>
-                    <p>Subtotal</p>
-                    <hr></hr>
-                    <p>${(item.quantity * +item.product.price).toFixed(2)}</p>
+                      ${item.product.price}
                     </Typography>
-                  </CardContent>
-                  <CardContent>
-                  </CardContent>
-                  <CardContent>
-                    <Typography variant='subtitle2' style={{marginLeft: '1.5rem'}}>
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between'
+                    }}
+                  >
+                    <Typography>
+                      Quantity: 
+                    </Typography>
+                    <Typography>
+                    {+item.quantity}
+                    </Typography>
+                  </div>
+                  <hr color='black'></hr>
+                  <hr color='black'></hr>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between'
+                    }}
+                  >
+                    <Typography>
+                      Subtotal: 
+                    </Typography>
+                    <Typography>
+                      ${(item.product.price * +item.quantity).toFixed(2)}
+                    </Typography>
+                  </div>
+                </div>
+                <div>
+                  <Typography 
+                    variant='subtitle2' 
+                    style={{
+                      marginLeft: '.5rem',
+                      color: '#26b7ff'
+                    }}
+                  >
                       Edit
-                    </Typography>
-
-                    <div className="quantity" style={{marginBottom: '1rem', marginRight: '1rem'}}>
-                      <input
-                        type="number"
-                        className="quantity"
-                        min="1"
-                        max={item.product.inventory < 10 ? item.product.inventory : 10} //this was just product.inventory : 10, I think adding item. has fixed the critical bug I encontered -Alex 10/29/21
-                        id={item.id}
-                        value={item.quantity}
-                        onChange={onChange}
-                      />
-                    </div>
-                    <Button onClick={()=> {deleteOrderItem(item.id)}} color='primary' variant='outlined' size='small'>Delete</Button>
-                    </CardContent>
-                </Box>
-              </Card>
-            </Grid>
+                  </Typography>
+                  <div className="quantity" style={{marginBottom: '1rem', marginRight: '1rem'}}>
+                    <input
+                      type="number"
+                      className="quantity"
+                      min="1"
+                      max={item.product.inventory < 10 ? item.product.inventory : 10}
+                      id={item.id}
+                      value={item.quantity}
+                      onChange={onChange}
+                    />
+                  </div>
+                  <div style={{marginRight: '2rem'}}>
+                  <Button onClick={()=>{deleteOrderItem(item.id)}} color='secondary' variant='outlined' size='small'>
+                    Delete
+                  </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Divider 
+              style={{
+                color: 'white',
+                borderRadius: 5,
+                boxShadow: '0 0px 7px 7px #ffffff',
+              }}
+            />
+            </>
           ))
         }
+        </div>
       </Grid>
         <Box className="container" style={{marginTop: '1rem', marginRight: '2rem'}}>
           <Typography variant='h6'>
